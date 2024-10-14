9a40206c392a960832d2c82743532526a6129620
hostem17 <vjjais17@gmail.com>
2024-10-14 18:10:59 +0530

    * session list fetch done * session subcription/unsubscription done * update location done

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import {
   MESSAGE,
   START_SESSION,
   STOP_SESSION,
+  SUBSCRIBE,
+  UNSUBSCRIBE,
   UPDATE_LOCATION,
 } from "./messages/messages.js";
 
@@ -21,7 +23,7 @@ function App() {
     { sessionId: 2 },
   ]);
   const [currentPosition, setCurrentPosition] = useState();
-  const [currentSession, setCurrentSession] = useState({ sessionId: "none" });
+  const [currentSession, setCurrentSession] = useState({ sessionId: null });
   const [mySession, setMySession] = useState();
 
   const startBroadCast = () => {
@@ -30,23 +32,20 @@ function App() {
       alert("connection not ready");
       return;
     }
-
     socket.send(JSON.stringify({ type: START_SESSION }));
-
-    // const id = navigator.geolocation.watchPosition((pos) => {
-    //   console.log(pos.coords);
-    //   socket.send(
-    //     JSON.stringify({
-    //       type: UPDATE_LOCATION,
-    //       payload: {
-    //         latitude: pos.coords.latitude,
-    //         longitude: pos.coords.longitude,
-    //       },
-    //     })
-    //   );
-    // });
-    // console.log(id);
   };
+
+  // Fetch sessions when socket is ready
+  useEffect(() => {
+    if (socket) {
+      socket.send(
+        JSON.stringify({
+          type: LIST_SESSIONS,
+        })
+      );
+    }
+  }, [socket]);
+
   // Broadcast if my session is set
   useEffect(() => {
     console.log("mysession  ", mySession);
@@ -57,8 +56,11 @@ function App() {
           JSON.stringify({
             type: UPDATE_LOCATION,
             payload: {
-              latitude: pos.coords.latitude,
-              longitude: pos.coords.longitude,
+              sessionId: mySession.sessionId,
+              location: {
+                latitude: pos.coords.latitude,
+                longitude: pos.coords.longitude,
+              },
             },
           })
         );
@@ -79,12 +81,27 @@ function App() {
     console.log(session);
     console.log(`selecting session - ${session?.sessionId}`);
     setCurrentSession(session);
+
+    if (!socket) {
+      alert("Server connection not ready!");
+    }
+
+    socket.send(
+      JSON.stringify({
+        type: SUBSCRIBE,
+        payload: {
+          sessionId: session.sessionId,
+        },
+      })
+    );
   };
 
   const handleMessage = (data) => {
+    console.log(data);
     switch (data?.type) {
       case START_SESSION:
-        setCurrentSession({ sessionId: data.payload.sessionId });
+        console.log("Setting my session", data.payload.sessionId);
+        setMySession({ sessionId: data.payload.sessionId });
         break;
       case STOP_SESSION:
         setCurrentSession({ sessionId: "none" });
@@ -94,7 +111,12 @@ function App() {
         setCurrentPosition([payload.latitude, payload.longitude]);
         break;
       case LIST_SESSIONS:
-        setSessions(data.payload);
+        setSessions(null);
+
+        const newSessions = data.payload.map((item) => ({
+          sessionId: item,
+        }));
+        setSessions(newSessions);
         break;
       case ERROR:
         break;
@@ -103,35 +125,67 @@ function App() {
     }
   };
 
+  // Create a websocket connection on load
   useEffect(() => {
     const ws = new WebSocket(wsURL);
-
     ws.onopen = () => {
       console.log("connnected to server");
+      setSocket(ws);
     };
 
     ws.onmessage = (data) => {
-      console.log("recieved data", JSON.parse(data.data));
-      handleMessage(data?.data);
+      const parsedData = JSON.parse(data.data);
+      console.log("recieved data", parsedData);
+      handleMessage(parsedData);
     };
 
     ws.onclose = () => {
       console.log("server disconnected");
     };
 
-    setSocket(ws);
+    // setSocket(ws);
   }, []);
 
   // To update data when selected session changes
-  useEffect(() => {}, [currentSession]);
+  useEffect(() => {
 
+    if (currentSession.sessionId) {
+      if (!socket) {
+        alert("Server connection not ready!");
+        return;
+      }
+
+      socket.send(
+        JSON.stringify({
+          type: SUBSCRIBE,
+          payload: currentSession,
+        })
+      );
+    }
+
+    return () => {
+      if (currentSession.sessionId) {
+        socket.send(
+          JSON.stringify({
+            type: UNSUBSCRIBE,
+            payload: currentSession,
+          })
+        );
+      }
+    };
+  }, [currentSession]);
+
+  const test = () => {
+    console.log("In test");
+    console.log(sessions);
+  };
   return (
     <div className="text-xs sm:text-base flex w-full h-svh max-h-svh justify-center items-center flex-col">
       <div className="flex w-3/4 h-full min-w-96 justify-around items-center flex-col ">
         <div className="flex w-full ">
           <div className="ALL_SESSIONS flex flex-1 bg-purple-300 h-16 justify-center items-center relative ">
             <div className=" currentSession text-center border-r-2 p-2 max-w-40 flex-1">
-              <span className="font-bold ">Current session</span>
+              <span className="font-bold ">Current session </span>
               <br />
               {currentSession.sessionId}
             </div>
@@ -143,26 +197,28 @@ function App() {
                   onClick={() => selectSession(session)}
                 >
                   <div className="flex-1 flex items-center h-full justify-center cursor-pointer">
-                    <h1 className="m-2">{session.sessionId}</h1>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="size-4 cursor-pointer"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
-                      />
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                      />
-                    </svg>
+                    <div className="flex flex-col items-center justify-center">
+                      <h1 className="m-2">{session.sessionId}</h1>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="size-4 cursor-pointer"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                        />
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                        />
+                      </svg>
+                    </div>
                   </div>
                 </div>
               ))}
@@ -174,6 +230,7 @@ function App() {
           >
             Start broadcast
           </button>
+          <button onClick={test}>TEST</button>
         </div>
         <div className="flex h-full w-full relative">
           <Map currentPosition={currentPosition} />
