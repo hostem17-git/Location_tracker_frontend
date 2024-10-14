f4cdac8b7af1c47b7ed195b5a7e86d4d8c60285d
hostem17 <vjjais17@gmail.com>
2024-10-15 00:55:10 +0530

    messages files added

---
+++ src/messages/messages.js
@@ -0,0 +1,8 @@
+export const START_SESSION = "start_broadcast";
+export const UPDATE_LOCATION = "update_location";
+export const STOP_SESSION = "stop_broadcast";
+export const SUBSCRIBE = "subscribe";
+export const UNSUBSCRIBE = "unsubscribe";
+export const LIST_SESSIONS = "list_sessions"
+export const MESSAGE = "message";
+export const ERROR = "error";
